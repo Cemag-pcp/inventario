a6a5ce9eaf00bed74f3843393e25f4a9f5e0922c
luan <luan640@gmail.com>
2022-12-27 12:01:02 -0300

    att_base

--- paginas/forms.py
+++ paginas/forms.py
@@ -4,8 +4,4 @@ from paginas.models import tb_balanco
 class empforms(forms.ModelForm):
     class Meta:
         model=tb_balanco
-        fields="__all__"    
-
-class InputForm(forms.ModelForm):
-    lista = forms.CharField(max_length = 200)
-    
+        fields="__all__"
--- paginas/models.py
+++ paginas/models.py
@@ -5,7 +5,7 @@ from django.db import models
 class tb_balanco(models.Model):
     codigo = models.CharField(max_length=50)
     descricao = models.CharField(max_length=150, verbose_name='Descrição')
-    familia = models.CharField(max_length=50, verbose_name='Familia')
+    familia = models.CharField(max_length=50, verbose_name='Lista')
     qt = models.IntegerField()
 
     class Meta:
