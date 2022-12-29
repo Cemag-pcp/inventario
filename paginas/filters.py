5dbad49756094ff1021c853ac6f6f922628033fc
luan <luan640@gmail.com>
2022-12-29 08:37:08 -0300

    bugs

--- paginas/filters.py
+++ paginas/filters.py
@@ -2,6 +2,8 @@ from paginas.models import tb_balanco
 import django_filters
 
 class UserFilter(django_filters.FilterSet):
+    codigo = django_filters.CharFilter(field_name = 'codigo', lookup_expr='contains')
+
     class Meta:
         model = tb_balanco
         fields = ['codigo', 'familia']
